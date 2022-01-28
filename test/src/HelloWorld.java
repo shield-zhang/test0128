b667746188aaf6d18eefa8f6ba9766763a669745
1120191562zhq <1120191562@bit.edu.cn>
2022-01-28 23:50:50 +0800

    test22

--- test/src/HelloWorld.java
+++ test/src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hhhhawq");
+        System.out.println("hhhhawq");
+        System.out.println("hhhhawq");
     }
 }
